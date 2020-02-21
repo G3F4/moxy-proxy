8907b53ba1c702e2f59e72ed2a46631d01c7aa9d
Kamil Grabek <kamil.grabek@allegro.pl>
2020-02-21 22:16:29 +0100

    travis plan change

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -3,6 +3,7 @@ import { Selector } from 'testcafe';
 const appHeaderSelector = Selector('h6').withText('Moxy Proxy');
 const endpointTabSelector = Selector('span').withText('ENDPOINTS');
 const addEndpointSelector = Selector('button').withText('ADD ENDPOINT');
+const urlPatternInputSelector = Selector('label').withText('URL pattern').parent().find('input');
 
 fixture `Fixture`
   .page `http://localhost:5000`;
@@ -11,5 +12,6 @@ test('User can add endpoint', async t => {
   await t.expect(appHeaderSelector.exists).ok({ timeout: 1000 });
   await t.click(endpointTabSelector);
   await t.click(addEndpointSelector);
+  await t.typeText(urlPatternInputSelector, 'test2/:id');
   await t.takeScreenshot('test.png');
 });
